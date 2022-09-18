92ff8f2d33501449492d9734fac6eec404260c8b
houbin <411797930@qq.com>
2022-09-18 08:37:49 +0800

    dev

---
+++ public/static/admin/js/business/specials.js
@@ -0,0 +1,45 @@
+define(["jquery", "easy-admin"], function ($, ea) {
+
+    var init = {
+        table_elem: '#currentTable',
+        table_render_id: 'currentTableRenderId',
+        index_url: 'business.specials/index',
+        add_url: 'business.specials/add',
+        edit_url: 'business.specials/edit',
+        delete_url: 'business.specials/delete',
+        export_url: 'business.specials/export',
+    };
+
+    var Controller = {
+        index: function () {
+            ea.table.render({
+                init: init,
+                cols: [[
+                    {type: "checkbox"},
+                    {field: 'id', width: 80, title: 'ID'},
+                    {field: 'title', minWidth: 80, title: '主標(繁)'},
+                    {field: 'sub_title', minWidth: 80, title: '副標(繁)'},
+                    {field: 'content', minWidth: 80, title: '內文', search:false},
+                    {field: 'start_date', minWidth: 80, title: '上架日', search:false},
+                    {field: 'large_photo_url', minWidth: 80, title: '專欄大圖', search: false, templet: ea.table.image},
+                    {field: 'small_photo_url', minWidth: 80, title: '專欄小圖', search: false, templet: ea.table.image},
+                    {field: 'source', minWidth: 80, title: '來源'},
+                    {field: 'valid', title: '狀態', width: 85, search: 'select', selectList: {2: '作廢', 1: '正常'}, templet: ea.table.switch},
+                    {field: 'createName', minWidth: 80, title: '建單人員', search:false},
+                    {field: 'create_time', minWidth: 120, title: '新增時間', search:false},
+                    {field: 'update_time', minWidth: 120, title: '修改時間', search:false},
+                    {width: 140, title: '操作', templet: ea.table.tool}
+                ]],
+            });
+
+            ea.listen();
+        },
+        add: function () {
+            ea.listen();
+        },
+        edit: function () {
+            ea.listen();
+        },
+    };
+    return Controller;
+});
