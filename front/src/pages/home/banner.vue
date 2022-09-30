6e0bb15883558167336b5617f8deaaeb14cb57eb
jony wu <jonywu@gmail.com>
2022-09-30 16:52:22 +0800

    UI_調整下方間距

--- front/src/pages/home/banner.vue
+++ front/src/pages/home/banner.vue
@@ -1,71 +1,69 @@
 <template>
-  <div>
-    <section class="banner-section">
-      <div class="container">
-        <div class="banner-content">
-          <div class="row flex-lg-row-reverse">
-            <!-- banner-pic-5 -->
-            <div class="col-12 mb-24 col-lg-6 mb-lg-0">
-              <a href="#" style="height: 100%;padding-bottom: 15px;">
-                <div class="pic banner-5">
-                  <img :src="bannerImg4?.banner_url" alt="">
-                </div>
-              </a>
-            </div>
-            <!-- banner-pic-1~4 -->
-            <div class="col-12 col-lg-6">
-              <div class="row">
-                <div class="col-12 mb-16">
-                  <!-- banner-pic-1 -->
-                  <a href="#">
-                    <div class="banner-1">
-                      <div class="pic">
-                        <img :src="bannerList?.banner_url" alt="">
-                      </div>
-                      <div class="text-area">
-                        <div class="title-group mb-16 mb-sm-24">
+  <section class="banner-section section mb-80">
+    <div class="container">
+      <div class="banner-content">
+        <div class="row flex-lg-row-reverse">
+          <!-- banner-pic-5 -->
+          <div class="col-12 mb-24 col-lg-6 mb-lg-0">
+            <a href="#" style="height: 100%;padding-bottom: 15px;">
+              <div class="pic banner-5">
+                <img :src="bannerImg4?.banner_url" alt="">
+              </div>
+            </a>
+          </div>
+          <!-- banner-pic-1~4 -->
+          <div class="col-12 col-lg-6">
+            <div class="row">
+              <div class="col-12 mb-16">
+                <!-- banner-pic-1 -->
+                <a href="#">
+                  <div class="banner-1">
+                    <div class="pic">
+                      <img :src="bannerList?.banner_url" alt="">
+                    </div>
+                    <div class="text-area">
+                      <div class="title-group mb-16 mb-sm-24">
 <!--                          <h2>Machina マキナ</h2>-->
 <!--                          <p>@Machina_NFT</p>-->
-                        </div>
-                        <p class="mb-4">{{bannerList?.description}}</p>
+                      </div>
+                      <p class="mb-4">{{bannerList?.description}}</p>
 <!--                        <small>Mar 30, 2022</small>-->
-                      </div>
                     </div>
-                  </a>
-                </div>
-                <!-- banner-pic-2 -->
-                <div class="col-6 col-sm-4">
-                  <a href="#">
-                    <div class="banner-2">
-                      <img :src="bannerImg1?.banner_url" alt="">
+                  </div>
+                </a>
+              </div>
+              <!-- banner-pic-2 -->
+              <div class="col-6 col-sm-4">
+                <a href="#">
+                  <div class="banner-2">
+                    <img :src="bannerImg1?.banner_url" alt="">
+                  </div>
+                </a>
+              </div>
+              <!-- banner-pic-3 -->
+              <div class="d-none d-sm-block col-sm-4">
+                <a href="#">
+                  <div class="banner-3">
+                    <img :src="bannerImg2?.banner_url" alt="">
+                  </div>
+                </a>
+              </div>
+              <!-- banner-pic-4 -->
+              <div class="col-6 col-sm-4">
+                <a href="#">
+                  <div class="banner-4">
+                    <div class="banner-4-pic">
+                      <img :src="bannerImg3?.banner_url" alt="">
                     </div>
-                  </a>
-                </div>
-                <!-- banner-pic-3 -->
-                <div class="d-none d-sm-block col-sm-4">
-                  <a href="#">
-                    <div class="banner-3">
-                      <img :src="bannerImg2?.banner_url" alt="">
-                    </div>
-                  </a>
-                </div>
-                <!-- banner-pic-4 -->
-                <div class="col-6 col-sm-4">
-                  <a href="#">
-                    <div class="banner-4">
-                      <div class="banner-4-pic">
-                        <img :src="bannerImg3?.banner_url" alt="">
-                      </div>
-                    </div>
-                  </a>
-                </div>
+                  </div>
+                </a>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </section>
-  </div>
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts">
