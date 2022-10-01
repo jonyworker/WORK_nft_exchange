8285b1b4557582da4e5a48065a6b67fdfaa2c3cc
houbin <411797930@qq.com>
2022-10-01 09:56:02 +0800

    dev

--- app/api/middleware/Auth.php
+++ app/api/middleware/Auth.php
@@ -3,7 +3,6 @@
 namespace app\api\middleware;
 
 use app\api\model\Member;
-use think\Exception;
 use app\Request;
 use think\exception\HttpException;
 
@@ -14,14 +13,14 @@ class Auth
     public function handle(Request $request, \Closure $next)
     {
         //登录验证，未登录跳转至登录界面
-        $token = $request->header('Authorization');
+        $token = $request->header('authorization');
         if(empty($token)) {
-            throw new HttpException('需要登录授权', 401);
+            throw new HttpException(401, '需要登录授权');
         }
 
         $info = Member::findByToken($token);
         if(!$info) {
-            throw new HttpException('请重新登录', 401);
+            throw new HttpException(401, '请重新登录');
         }
 
        $request->withMiddleware(['userId' => $info['id']]);
