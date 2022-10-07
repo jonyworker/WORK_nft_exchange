34a99661bab7b42c3e1c5f8aa80b7e63bec5d915
jony wu <jonywu@gmail.com>
2022-10-07 10:52:41 +0800

    UI_CK4重新安裝測試中

--- public/static/plugs/ckeditor4/config.js
+++ public/static/plugs/ckeditor4/config.js
@@ -1,57 +1,26 @@
-CKEDITOR.editorConfig = function (config) {
-    config.language = 'zh-cn';
-    config.image_previewText = ' ';
-    config.height = 500;
-    config.width = 'auto';
+/**
+ * @license Copyright (c) 2003-2022, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ */
 
-	// config.extraPlugins = 'image2';
-
-	// config.extraPlugins =  'simage';  //to enable to plugin
-	// config.imageUploadURL = "/admin/ajax/uploadEditor";
-	// config.dataParser = func(data);
-
-    // config.toolbarGroups = [
-    //     {name: 'document', groups: ['mode', 'document', 'doctools']},
-    //     {name: 'styles', groups: ['Font', 'FontSize']},
-    //     {name: 'colors'},
-    //     {name: 'basicstyles', groups: ['basicstyles', 'cleanup']},
-    //     {name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi']},
-    //     {name: 'insert'},
-    //     {name: 'others'},
-    //     {name: 'forms'},
-    //     {name: 'links'},
-    //     {name: 'clipboard', groups: ['clipboard', 'undo']},
-    //     { name: 'insert', groups: [ 'EasyImageUpload' ] },
-    //     {name: 'tools'},
-    // ];
-    config.toolbarGroups = [
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+	config.toolbarGroups = [
 		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
 		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
 		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
 		{ name: 'forms', groups: [ 'forms' ] },
-		
 		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
 		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
 		{ name: 'links', groups: [ 'links' ] },
 		{ name: 'insert', groups: [ 'insert' ] },
-		
 		{ name: 'styles', groups: [ 'styles' ] },
 		{ name: 'colors', groups: [ 'colors' ] },
 		{ name: 'tools', groups: [ 'tools' ] },
 		{ name: 'others', groups: [ 'others' ] },
-		{ name: 'about', groups: [ 'about' ] },
-
-		// { name: 'image2', groups: [ 'image2' ] },
-		// { name: 'simage', groups: [ 'simage' ] }
+		{ name: 'about', groups: [ 'about' ] }
 	];
-    config.filebrowserImageUploadUrl = config.filebrowserImageUploadUrl || "/admin/ajax/uploadEditor";
-
-    config.removeButtons = 'Underline,Subscript,Superscript';
-
-    config.format_tags = 'p;h1;h2;h3;pre';
-
-    // config.removeDialogTabs = 'image:advanced;link:advanced';
-    config.removeButtons = 'Save,NewPage,Preview,Print,Templates,Cut,Copy,Paste,PasteText,PasteFromWord,Find,Replace,SelectAll,Scayt,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Subscript,Superscript,RemoveFormat,Outdent,Indent,CreateDiv,BidiLtr,BidiRtl,Language,JustifyLeft,JustifyCenter,JustifyRight,JustifyBlock,Table,HorizontalRule,Smiley,SpecialChar,PageBreak,Iframe,Flash,FontSize,Font,TextColor,BGColor,Maximize,ShowBlocks,About,Styles,Anchor';
-
-    config.font_names = '微软雅黑/Microsoft YaHei;宋体/SimSun;新宋体/NSimSun;仿宋/FangSong;楷体/KaiTi;黑体/SimHei;' + config.font_names;
+	config.removeButtons = 'Save,NewPage,ExportPdf,Preview,Print,Templates,Cut,Copy,Replace,Find,Paste,PasteText,PasteFromWord,SelectAll,Scayt,Form,Checkbox,Radio,TextField,Textarea,Select,Button,HiddenField,Superscript,Subscript,CopyFormatting,RemoveFormat,Indent,Outdent,JustifyLeft,JustifyCenter,JustifyRight,JustifyBlock,Language,BidiRtl,BidiLtr,Anchor,Table,HorizontalRule,Smiley,SpecialChar,PageBreak,Iframe,SImage,FontSize,Font,Styles,TextColor,BGColor,ShowBlocks,Maximize,About';
 };
--- public/static/plugs/ckeditor4/styles.js
+++ public/static/plugs/ckeditor4/styles.js
@@ -1,5 +1,5 @@
 ﻿/**
- * Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
+ * Copyright (c) 2003-2022, CKSource Holding sp. z o.o. All rights reserved.
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
@@ -33,37 +33,82 @@ CKEDITOR.stylesSet.add( 'default', [
 	{ name: 'Address',			element: 'address' },
 	*/
 
+	{ name: 'Italic Title',		element: 'h2', styles: { 'font-style': 'italic' } },
+	{ name: 'Subtitle',			element: 'h3', styles: { 'color': '#aaa', 'font-style': 'italic' } },
+	{
+		name: 'Special Container',
+		element: 'div',
+		styles: {
+			padding: '5px 10px',
+			background: '#eee',
+			border: '1px solid #ccc'
+		}
+	},
+
+	/* Inline styles */
+
+	// These are core styles available as toolbar buttons. You may opt enabling
+	// some of them in the Styles drop-down list, removing them from the toolbar.
+	// (This requires the "stylescombo" plugin.)
+	/*
+	{ name: 'Strong',			element: 'strong', overrides: 'b' },
+	{ name: 'Emphasis',			element: 'em'	, overrides: 'i' },
+	{ name: 'Underline',		element: 'u' },
+	{ name: 'Strikethrough',	element: 'strike' },
+	{ name: 'Subscript',		element: 'sub' },
+	{ name: 'Superscript',		element: 'sup' },
+	*/
+
+	{ name: 'Marker',			element: 'span', attributes: { 'class': 'marker' } },
+
+	{ name: 'Big',				element: 'big' },
+	{ name: 'Small',			element: 'small' },
+	{ name: 'Typewriter',		element: 'tt' },
+
+	{ name: 'Computer Code',	element: 'code' },
+	{ name: 'Keyboard Phrase',	element: 'kbd' },
+	{ name: 'Sample Text',		element: 'samp' },
+	{ name: 'Variable',			element: 'var' },
+
+	{ name: 'Deleted Text',		element: 'del' },
+	{ name: 'Inserted Text',	element: 'ins' },
+
+	{ name: 'Cited Work',		element: 'cite' },
+	{ name: 'Inline Quotation',	element: 'q' },
+
+	{ name: 'Language: RTL',	element: 'span', attributes: { 'dir': 'rtl' } },
+	{ name: 'Language: LTR',	element: 'span', attributes: { 'dir': 'ltr' } },
 
 	/* Object styles */
 
-	// {
-	// 	name: 'Styled Image (left)',
-	// 	element: 'img',
-	// 	attributes: { 'class': 'left' }
-	// },
+	{
+		name: 'Styled Image (left)',
+		element: 'img',
+		attributes: { 'class': 'left' }
+	},
 
-	// {
-	// 	name: 'Styled Image (right)',
-	// 	element: 'img',
-	// 	attributes: { 'class': 'right' }
-	// },
+	{
+		name: 'Styled Image (right)',
+		element: 'img',
+		attributes: { 'class': 'right' }
+	},
 
-	// {
-	// 	name: 'Compact Table',
-	// 	element: 'table',
-	// 	attributes: {
-	// 		cellpadding: '5',
-	// 		cellspacing: '0',
-	// 		border: '1',
-	// 		bordercolor: '#ccc'
-	// 	},
-	// 	styles: {
-	// 		'border-collapse': 'collapse'
-	// 	}
-	// },
+	{
+		name: 'Compact Table',
+		element: 'table',
+		attributes: {
+			cellpadding: '5',
+			cellspacing: '0',
+			border: '1',
+			bordercolor: '#ccc'
+		},
+		styles: {
+			'border-collapse': 'collapse'
+		}
+	},
 
-	// { name: 'Borderless Table',		element: 'table',	styles: { 'border-style': 'hidden', 'background-color': '#E6E6FA' } },
-	// { name: 'Square Bulleted List',	element: 'ul',		styles: { 'list-style-type': 'square' } },
+	{ name: 'Borderless Table',		element: 'table',	styles: { 'border-style': 'hidden', 'background-color': '#E6E6FA' } },
+	{ name: 'Square Bulleted List',	element: 'ul',		styles: { 'list-style-type': 'square' } },
 
 	/* Widget styles */
 
