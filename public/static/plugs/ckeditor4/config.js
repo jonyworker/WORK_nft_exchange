05798ac3d6dff94242180f867f2f0518a450c9d5
jony wu <jonywu@gmail.com>
2022-10-07 16:33:37 +0800

    UI_增加圖片上傳器config設定

--- public/static/plugs/ckeditor4/config.js
+++ public/static/plugs/ckeditor4/config.js
@@ -23,4 +23,20 @@
 		{ name: 'about', groups: [ 'about' ] }
 	];
 	config.removeButtons = 'Save,NewPage,ExportPdf,Preview,Print,Templates,Cut,Copy,Replace,Find,Paste,PasteText,PasteFromWord,SelectAll,Scayt,Form,Checkbox,Radio,TextField,Textarea,Select,Button,HiddenField,Superscript,Subscript,CopyFormatting,RemoveFormat,Indent,Outdent,JustifyLeft,JustifyCenter,JustifyRight,JustifyBlock,Language,BidiRtl,BidiLtr,Anchor,Table,HorizontalRule,Smiley,SpecialChar,PageBreak,Iframe,SImage,FontSize,Font,Styles,TextColor,BGColor,ShowBlocks,Maximize,About';
+
+	config.format_tags = 'p;h2;h3;div';
+	
+	config.filebrowserImageUploadUrl = config.filebrowserImageUploadUrl || "/admin/ajax/uploadEditor";
+	config.removeDialogTabs = 'image:advanced;link:advanced';
+
+	config.extraPlugins = 'simage';  //to enable to plugin
+	config.imageUploadURL = ''; // 所需上传图片的URL
+	config.dataParser = function (data) {
+		if (data){
+			var keys = Object.keys(data)
+			return data[keys[0]].url
+		}
+	}
 };
+
+
