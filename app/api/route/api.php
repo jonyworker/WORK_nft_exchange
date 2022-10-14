2c4d80ec74da217f88c10d9e8f3446cddfae2f46
houbin <411797930@qq.com>
2022-10-14 10:44:20 +0800

    dev

--- app/api/route/api.php
+++ app/api/route/api.php
@@ -26,7 +26,7 @@ Route::post('member/nft/add','MemberNFT/add')->middleware(Auth::class);
 //取得項目主頁資訊 GET api/collection/index
 Route::get('collection/index','Collections/index')->middleware(OptionalAuth::class);
 //取得項目主頁NFT卡片 POST api/collection/nft
-Route::post('collection/nft','Collections/nft');
+Route::post('collection/nft','Collections/nft')->middleware(OptionalAuth::class);
 
 //图片上传 POST /api/upload/image 字段file
 Route::post('upload/image','Upload/image')->middleware(Auth::class);
