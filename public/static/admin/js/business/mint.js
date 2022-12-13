0578b824dcde006d28a112abb019066cc3c01789
韩顺畴 <1650086112@qq.com>
2022-12-13 17:30:47 +0800

    1213 Q上反馈 修改时间逻辑，原本的有误

--- public/static/admin/js/business/mint.js
+++ public/static/admin/js/business/mint.js
@@ -98,7 +98,7 @@ define(["jquery", "easy-admin"], function ($, ea) {
         date.setHours(date.getHours() + time);
 
         var y = date.getFullYear();
-        var m = (date.getMonth() + 1) < 10 ? ("0" + (date.getMonth() + 1)) : (date.getMinutes() + 1);
+        var m = (date.getMonth() + 1) < 10 ? ("0" + (date.getMonth() + 1)) : (date.getMonth() + 1);
         var d = date.getDate() < 10 ? ("0" + date.getDate()) : date.getDate();
         var h = date.getHours() < 10 ? ('0' + date.getHours()) : date.getHours()
         var f = date.getMinutes() < 10 ? ('0' + date.getMinutes()) : date.getMinutes()
